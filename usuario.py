7d45936e175fe12806f0f3b6831b7e8d13660260
Saulo <218009211+saulohpm@users.noreply.github.com>
2026-01-19 14:55:08 -0300

    Versão 2.5
    users config

---
+++ usuario.py
@@ -0,0 +1,38 @@
+import json
+import os
+import utils
+
+usuarios = []
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ARQUIVO_USUARIOS = os.path.join(BASE_DIR, "usuarios.json")
+
+def salvar_usuarios(lista):
+    with open(ARQUIVO_USUARIOS, "w", encoding="utf-8") as f:
+        json.dump(lista, f, ensure_ascii=False, indent=4)
+
+
+def carregar_usuarios():
+    if os.path.exists(ARQUIVO_USUARIOS):
+        with open(ARQUIVO_USUARIOS, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
+
+def cadastrodeusuario(usuarios):
+    utils.limpar_tela()
+    if not usuarios:
+        utils.titulo("🌱  Bem-vindo(a) ao Sistema! 🌱")
+        nome = input("✏️  Digite seu nome ou como quer ser chamado(a): ")
+
+        usuarios.append({"nome": nome})
+        salvar_usuarios(usuarios)
+
+        print(f"\n🎉  Obrigado, {nome}! Você será redirecionado(a) para o menu do Sistema em instantes!")
+
+        utils.pausa_tempo()
+    else:
+        return
+
+def apagarusuario(usuarios):
+    usuarios.pop(0)
+    return
