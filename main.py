9bd04f880e878baa200e2250c1b48daaf22c028f
Saulo <218009211+saulohpm@users.noreply.github.com>
2026-01-19 14:54:21 -0300

    Versão 2.5
    Integrate user management into main application

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from plantacoes import menu
+from usuario import carregar_usuarios, cadastrodeusuario
 import utils
 
 if __name__ == "__main__":
+
+    usuarios = carregar_usuarios()
+    cadastrodeusuario(usuarios)
+
     plantacoes = utils.carregar_dados()
-    menu(plantacoes)
+    menu(plantacoes, usuarios)
